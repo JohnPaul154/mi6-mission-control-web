120cc9f7837f3bc3d92868b48184cb5e4b2ead76
JohnPaul154 <johnpaul.verzosa@neu.edu.ph>
2025-02-28 01:46:18 +0800

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during builds
+  },
 };
 export default nextConfig;
